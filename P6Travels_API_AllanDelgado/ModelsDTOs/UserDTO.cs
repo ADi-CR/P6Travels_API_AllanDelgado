2f1d07c2c37083fde23d53e73f0dcb1e91ee6db2
Allan Delgado C <allandelgadoc@gmail.com>
2024-06-13 20:58:00 -0600

    Semana 5

---
+++ P6Travels_API_AllanDelgado/ModelsDTOs/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace P6Travels_API_AllanDelgado.ModelsDTOs
+{
+    //UN DTO (Data Tranfer Object) es un tipo 
+    //de versión "recortada" de un modelo particular
+    //donde se pretende quitar todas las complejidades
+    //generadas en los modelos a partir del ORM (Entity
+    //Framework en nuestro caso). 
+    
+    //El propósito también puede ser ocultar la estructura 
+    //original de los modelos al equipo de desarrollo 
+    //Front End ya sea porque no es necesaria visualizar
+    //el modelo completo o porque no se debe mostrar la 
+    //estructura original de las tablas a nivel de 
+    //bases de datos. 
+
+    public class UserDTO
+    {
+
+
+
+    }
+}
